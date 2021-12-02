cf18003168599c9c06c04d860994cb3570c6d9eb
Patrycja Miga <patrycjamiga3@gmail.com>
2021-12-01 21:41:55 -0500

    filtered-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //feature 1
 import React from "react";
+import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json"
 
@@ -14,6 +15,40 @@ class App extends React.Component {
     };
   }
 
+  sortProducts = (event) => {
+
+    const sort = event.target.value;
+
+    this.setState(state => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a, b) =>
+        sort === "lowest"
+          ? a.price > b.price
+            ? 1
+            : -1
+          : a.price > b.price
+            ? -1
+            : 1
+      ),
+    }))
+  }
+
+  filterProducts = (event) => {
+
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products })
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+
+
+  }
+
   render() {
     return (
       <div className="App" >
@@ -23,6 +58,11 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts} />
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">
